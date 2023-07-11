a6773cb029170bc4206e724823e140cc61bb15da
Jin Bal <gurjinder.bal@digital.hmrc.gov.uk>
2023-07-11 12:17:11 +0100

    GG-7155 lib upgrades coverage bump to 85%

--- project/CodeCoverageSettings.scala
+++ project/CodeCoverageSettings.scala
@@ -17,7 +17,7 @@ object CodeCoverageSettings {
 
   val settings: Seq[Setting[_]] = Seq(
     ScoverageKeys.coverageExcludedPackages := excludedPackages.mkString(";"),
-    ScoverageKeys.coverageMinimumStmtTotal := 80,
+    ScoverageKeys.coverageMinimumStmtTotal := 85,
     ScoverageKeys.coverageFailOnMinimum := true,
     ScoverageKeys.coverageHighlighting := true
   )
