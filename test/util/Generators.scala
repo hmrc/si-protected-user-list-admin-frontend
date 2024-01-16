17c9c855b386bad4bc47ce4464b33fa5336aa14f
Modestas Rukšnaitis <46432428+ModisR@users.noreply.github.com>
2024-01-16 15:25:18 +0000

    GG-7540 eliminated remaining Entry references in UTs & Generators; gaps to be covered by ITs

--- test/util/Generators.scala
+++ test/util/Generators.scala
@@ -55,33 +55,6 @@ trait Generators {
 
   val sautrGen: Gen[SaUtr] = Gen.const(new SaUtrGenerator().nextSaUtr)
 
-  val entryGen: Gen[Entry] = for {
-    action             <- Gen.oneOf(InputForms.addEntryActionBlock, InputForms.addEntryActionLock)
-    nino               <- Gen.some(ninoGen.map(_.nino))
-    sautr              <- Gen.some(sautrGen.map(_.utr))
-    identityProvider   <- Gen.some(nonEmptyStringGen)
-    identityProviderId <- Gen.some(nonEmptyStringGen)
-    group              <- Gen.some(nonEmptyStringOfGen(groupMaxLength))
-    addedByTeam        <- nonEmptyStringGen
-    updatedByTeam      <- Gen.some(nonEmptyStringGen)
-    updatedByUser      <- Gen.some(nonEmptyStringGen)
-    addedByUser        <- Gen.some(nonEmptyStringGen)
-  } yield Entry(
-    action = action,
-    nino = nino,
-    sautr = sautr,
-    identityProvider = identityProvider,
-    identityProviderId = identityProviderId,
-    group = group,
-    addedByTeam = addedByTeam,
-    updatedByTeam = updatedByTeam,
-    updatedByUser = updatedByUser,
-    addedByUser = addedByUser
-  )
-
-  val validRequestEntryGen: Gen[Entry] = entryGen.map(_.copy(addedByUser = None, updatedByUser = None, action = InputForms.addEntryActionLock))
-  val validEditEntryGen = entryGen.map(_.copy(addedByUser = None, updatedByUser = None, action = InputForms.addEntryActionLock))
-
   val siProtectedUserConfigGen: Gen[SiProtectedUserConfig] = for {
     num               <- Gen.chooseNum(1, 10)
     addedByTeams      <- Gen.listOfN(num, nonEmptyStringGen)
