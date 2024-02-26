310a8a195d61c382fb7655d5f949319648a14c69
ianmcnicholas <ian.mcnicholas@digital.hmrc.gov.uk>
2024-02-26 15:36:48 +0000

    GG-7650 : Tudor Crown upgrade to play-frontend-hmrc

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -5,7 +5,7 @@ object AppDependencies {
 
   private val compile = Seq(
     "uk.gov.hmrc" %% "bootstrap-frontend-play-28"    % bootstrapVersion,
-    "uk.gov.hmrc" %% "play-frontend-hmrc"            % "7.4.0-play-28",
+    "uk.gov.hmrc" %% "play-frontend-hmrc-play-28"    % "8.5.0",
     "uk.gov.hmrc" %% "play-conditional-form-mapping" % "1.13.0-play-28"
   )
 
