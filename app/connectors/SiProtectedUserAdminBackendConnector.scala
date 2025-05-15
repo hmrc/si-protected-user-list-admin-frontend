2871089e6777d8a8a9bfeb624272e3cd6e17181d
dwalters <32767569+david-walters-HMRC@users.noreply.github.com>
2025-05-15 10:30:34 +0100

    GG-8640 add 'name' to three older audit events

--- app/connectors/SiProtectedUserAdminBackendConnector.scala
+++ app/connectors/SiProtectedUserAdminBackendConnector.scala
@@ -24,12 +24,13 @@ import play.api.Logging
 import play.api.http.Status.*
 import play.api.libs.json.Json
 import play.api.libs.ws.writeableOf_JsValue
+import play.api.mvc.AnyContent
 import uk.gov.hmrc.http.HttpReads.Implicits.*
 import uk.gov.hmrc.http.client.HttpClientV2
 import uk.gov.hmrc.http.{ConflictException, HeaderCarrier, HttpResponse, NotFoundException, StringContextOps, UpstreamErrorResponse}
 import uk.gov.hmrc.play.audit.AuditExtensions.auditHeaderCarrier
 import uk.gov.hmrc.play.audit.http.connector.AuditConnector
-import uk.gov.hmrc.play.audit.model.ExtendedDataEvent
+import uk.gov.hmrc.play.audit.model.{DataEvent, ExtendedDataEvent}
 
 import javax.inject.{Inject, Named, Singleton}
 import scala.concurrent.{ExecutionContext, Future}
@@ -136,6 +137,7 @@ class SiProtectedUserAdminBackendConnector @Inject() (
             tags        = hc.toAuditTags(s"HMRC - SI Protected User List Admin - $action - $transactionType", request.path),
             detail = Json.obj(
               "pid"   -> request.getUserPid,
+              "name"  -> request.nameOpt.map(name => s"${name.name.getOrElse("-")} ${name.lastName.getOrElse("-")}".trim),
               "group" -> (if (record.body.group.isBlank) "-" else record.body.group),
               "team"  -> team,
               "entry" -> {
