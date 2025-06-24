89771254a19a33c093bf7bf9e01b0858ef001a5d
aadil.akhtar <48994751+AadilAkhtar@users.noreply.github.com>
2025-06-24 11:05:37 +0100

    added more upgrades

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -1,13 +1,13 @@
 import sbt.*
 
 object AppDependencies {
-  private val bootstrapVersion = "9.11.0"
+  private val bootstrapVersion = "9.13.0"
   private val scalaTestPlusVersion = "3.2.19.0"
 
   private val compile = Seq(
     "uk.gov.hmrc" %% "bootstrap-frontend-play-30"            % bootstrapVersion,
     "uk.gov.hmrc" %% "play-frontend-hmrc-play-30"            % "12.6.0",
-    "uk.gov.hmrc" %% "play-conditional-form-mapping-play-30" % "3.2.0"
+    "uk.gov.hmrc" %% "play-conditional-form-mapping-play-30" % "3.3.0"
   )
 
   private val test = Seq(
