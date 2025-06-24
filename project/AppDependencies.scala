60b78d362f6b7439e9b3fc5b4440a642761e066a
aadil.akhtar <48994751+AadilAkhtar@users.noreply.github.com>
2025-06-24 10:54:33 +0100

    updated to play-hmrc-frontend 12.6.0

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -6,7 +6,7 @@ object AppDependencies {
 
   private val compile = Seq(
     "uk.gov.hmrc" %% "bootstrap-frontend-play-30"            % bootstrapVersion,
-    "uk.gov.hmrc" %% "play-frontend-hmrc-play-30"            % "12.0.0",
+    "uk.gov.hmrc" %% "play-frontend-hmrc-play-30"            % "12.6.0",
     "uk.gov.hmrc" %% "play-conditional-form-mapping-play-30" % "3.2.0"
   )
 
